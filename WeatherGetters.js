0d7055a5144db98f9d2aaf293b09a3b1a76b2023
NandreyN <andrey.naumovich@mail.ru>
2015-09-12 22:48:23 +0300

    Current temperature info

---
+++ WeatherGetters.js
@@ -0,0 +1,26 @@
+﻿function fromKelvinToFarengeit(temperature) {
+    return Math.round(temperature - 273);
+}
+
+function getCurrentWeather() {
+    $('#panel').hide('fast');
+    var x = new XMLHttpRequest();
+    x.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=Minsk", true);
+    x.onload = function () {
+        var object = JSON.parse(x.responseText);
+        console.log(object);
+        document.getElementById('weather-heading').innerText = "Weather in " + object['name'].toString() + ":";
+        // weather.o.icon
+        document.getElementById('weather-picture').src = "Weather_Pictures/" + object['weather']['0'].icon + ".png";
+        var weatherBody = document.getElementById('weather-body');
+        weatherBody.innerText = fromKelvinToFarengeit(object['main']['temp']) + " °C\t\t" + object['weather'][0].description;
+        weatherBody.style.fontSize = '30px';
+
+        $('#panel').show(1000);
+    }
+    x.onerror = function () {
+        console.log("Error" + x.responseText)
+        $('#panel').show('slow');
+    }
+    x.send(null);
+}
---
+++ mainScript.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+$('#alert').hide();
+$('#info').hide();
+$('#danger').hide();
+$('#warning').hide();
+getCurrentWeather();
