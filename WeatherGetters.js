d94e06a28173878b9a8923f13960534ed3ff16c4
NandreyN <andrey.naumovich@mail.ru>
2015-09-18 00:17:15 +0300

    Filling additional iforation block

--- WeatherGetters.js
+++ WeatherGetters.js
@@ -12,15 +12,27 @@ function getCurrentWeather() {
         var bimage = 'Background_Pictures/' + object['weather']['0'].icon + ".jpg"
         var elemCollection = document.getElementsByClassName('background-image');
         for (var i = 0; i < elemCollection.length; i++) {
-            elemCollection[i].style.backgroundImage = "url('"+bimage+"')";
+            elemCollection[i].style.backgroundImage = "url('" + bimage + "')";
         }
 
+        var getAdditionalProps = function (mainBlock, propNames) { // block name and dictionary with props
+            var string = "";
+            for (prop in propNames)
+                string += prop + ": " + object[mainBlock.toString()][(prop).toLowerCase()].toString() + propNames[prop] + '\n';
+            return string;
+        }
+
+        var buildWeatherBody = function () {
+            var weatherBody = document.getElementById('weather-body');
+            weatherBody.innerText = fromKelvinToFarengeit(object['main']['temp']) + " °C\t\t";
+            weatherBody.style.fontStyle = 'italic';
+            weatherBody.style.fontSize = '30px';
+        }
+
+        buildWeatherBody();
         document.getElementById('weather-picture').src = "Weather_Pictures/" + object['weather']['0'].icon + ".png";
-        var weatherBody = document.getElementById('weather-body');
-        weatherBody.innerText = fromKelvinToFarengeit(object['main']['temp']) + " °C\t\t";
-        weatherBody.style.fontStyle = 'italic';
-        weatherBody.style.fontSize = '30px';
         document.getElementById('wdesc').innerText = object['weather'][0].description;
+        document.getElementById('info').innerText = getAdditionalProps("main", { "Pressure": "\thPa", "Humidity": "\t%" });
 
         $('#panel').show(1000);
     }
