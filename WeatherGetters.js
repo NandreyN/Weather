9c8da6211ddf855d7c7c6761e35bb7d2f791b7be
NandreyN <andrey.naumovich@mail.ru>
2015-09-23 22:12:23 +0300

    Preparing for foreacst

--- WeatherGetters.js
+++ WeatherGetters.js
@@ -2,35 +2,19 @@
     return Math.round(temperature - 273);
 }
 
-function getCurrentWeather() {
+function getWeather() {
     $('#panel').hide('fast');
     var x = new XMLHttpRequest();
     x.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=Minsk", true);
     x.onload = function () {
         var object = JSON.parse(x.responseText);
         console.log(object);
-
-        var getForecast = function ()
-        {
-            var req = new XMLHttpRequest();
-            req.open("GET", "http://api.openweathermap.org/data/2.5/forecast?q=Minsk", true);
-            req.onload = function () {
-                var forecast = JSON.parse(req.responseText);
-                console.log(forecast);
-            }
-            req.onerror = function () {
-                alert("error");
-            }
-            req.send(null);
-        }
-
         var getBackgroundID = function () {
             var bimage = 'Background_Pictures/' + object['weather']['0'].icon + ".jpg"
             var elemCollection = document.getElementsByClassName('background-image');
             for (var i = 0; i < elemCollection.length; i++) {
                 elemCollection[i].style.backgroundImage = "url('" + bimage + "')";
             }
-            getForecast();
         }
         var currentWeatherScenario = function () {
             getBackgroundID();
@@ -61,11 +45,13 @@ function getCurrentWeather() {
         }
 
         var page = window.location.toString().toLowerCase();
-        var funcsList = { "bootstrapsite": currentWeatherScenario, "forecast": getBackgroundID };
+        var funcsList = { "bootstrapsite": [currentWeatherScenario], "forecast": [getBackgroundID, getForecast] };
         for (name in funcsList)
             if (page.indexOf(name) != -1) {
-                funcsList[name]();
-                return;
+                for (var i = 0; i < funcsList[name].length;i++)
+                {
+                    funcsList[name][i]();
+                }
             }
     }
     x.onerror = function () {
@@ -74,4 +60,16 @@ function getCurrentWeather() {
             $('#panel').show('slow');
     }
     x.send(null);
+}
+function getForecast() {
+    var req = new XMLHttpRequest();
+    req.open("GET", "http://api.openweathermap.org/data/2.5/forecast?q=Minsk", true);
+    req.onload = function () {
+        var forecast = JSON.parse(req.responseText);
+        console.log(forecast);
+    }
+    req.onerror = function () {
+        alert("error");
+    }
+    req.send(null);
 }
